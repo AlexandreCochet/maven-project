fd03265635df44dfe0ee06ce1ec4021d76783caf
AlexandreCochet <47112710+AlexandreCochet@users.noreply.github.com>
2019-01-29 11:03:36 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,20 +1,20 @@
 package com.example;
 
 /**
-*this is a class
+*this is a class.
 */
 
 public class Greeter {
 /**
-*this is a constructor
+*this is a constructor.
 */
   
   public Greeter() {
 
   }
   /*
-  * @param : some name of someone
-  * @return : greeting string
+  *@param some name of someone.
+  *@return greeting string.
   */
   public final String greet(final String someone) {
     return String.format("Hello alexandre, %s!", someone);
