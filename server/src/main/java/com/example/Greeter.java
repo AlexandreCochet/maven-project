360384c44a11e062d835e214df40f9043628ae06
AlexandreCochet <47112710+AlexandreCochet@users.noreply.github.com>
2019-01-29 11:20:37 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,15 +6,14 @@ public class Greeter {
 /**
 *this is a constructor.
 */
-  public Greeter() 
-  {
+  public Greeter() {
+    
   }
   /**
   * {@param} some name of someone
   * {@return} greeting string
   */
-  public final String greet(final String someone) 
-  {
+  public final String greet(final String someone) {
     return String.format("Hello alexandre, %s!", someone);
   }
 }
