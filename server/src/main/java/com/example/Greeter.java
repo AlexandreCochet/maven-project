adc4e106eb6aa11336ef3297fae0c725876a6194
AlexandreCochet <47112710+AlexandreCochet@users.noreply.github.com>
2019-01-29 10:42:11 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,18 @@
 package com.example;
 
+/**
+*this is a class
+*/
 
 public class Greeter {
-
+/**
+*this is a constructor
+*/
   
   public Greeter() {
 
   }
-
+  //TO DO : add javadoc comment
   public final String greet(final String someone) {
     return String.format("Hello alexandre, %s!", someone);
   }
